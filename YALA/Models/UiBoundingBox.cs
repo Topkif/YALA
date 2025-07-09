a5dd09192eb9ac5d505f0f973bb568b1295b346b
Topkif <marin.dhai@gmail.com>
2025-07-09 23:04:53 +0200

    Added nice colors and first UI

---
+++ YALA/Models/UiBoundingBox.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YALA.Models;
+public partial class UiBoundingBox
+{
+	public double X { get; set; }
+	public double Y { get; set; }
+	public double Width { get; set; }
+	public double Height { get; set; }
+}
+
+/*using CommunityToolkit.Mvvm.ComponentModel;
+
+public partial class UiBoundingBox : ObservableObject
+{
+    [ObservableProperty] private double x;
+    [ObservableProperty] private double y;
+    [ObservableProperty] private double width;
+    [ObservableProperty] private double height;
+}
+*/
